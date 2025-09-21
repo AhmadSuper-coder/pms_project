3416a630b7729a215921ea0d34dd65545c45645d
the@coder <ahmadraza001912@gmail.com>
2025-09-21 20:58:44 +0530

    added allowed host

--- pms_backend/settings.py
+++ pms_backend/settings.py
@@ -133,6 +133,9 @@ AUTH_USER_MODEL = "accounts.PMSUser"
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
